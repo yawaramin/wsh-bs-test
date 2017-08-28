84ae797aa3a2155a49351c3e915af73d4cd8a04a
Yawar Amin <yawar.amin@mediative.com>
2017-08-28 13:49:37 -0400

    Add Wsh.Scripting.Dictionary.CompareMode

--- src/wsh_Scripting_Dictionary.ml
+++ src/wsh_Scripting_Dictionary.ml
@@ -1,9 +1,20 @@
 type t
 
 let make () = Wsh_ActiveXObject.make "Scripting.Dictionary"
-external add : string -> 'a -> unit = "" [@@bs.send.pipe: t]
+external add : string -> 'a -> unit = "Add" [@@bs.send.pipe: t]
+
+module CompareMode = struct
+  let binary = 0
+  let text = 1
+  let database = 2
+
+  external get : t -> int = "CompareMode" [@@bs.get]
+  external set : t -> int -> unit = "CompareMode" [@@bs.set]
+end
+
 external count : t -> int = "Count" [@@bs.get]
 external exists : string -> bool = "Exists" [@@bs.send.pipe: t]
 external item : string -> 'a = "Item" [@@bs.send.pipe: t]
+external removeAll : t -> unit = "RemoveAll" [@@bs.send]
 external remove_exn : string -> unit = "Remove" [@@bs.send.pipe: t]
 
--- src/wsh_Scripting_Dictionary.mli
+++ src/wsh_Scripting_Dictionary.mli
@@ -4,7 +4,17 @@ type t
 val make : unit -> t
 
 (** https://msdn.microsoft.com/en-us/library/5h92h863(v=vs.84).aspx *)
-external add : string -> 'a -> unit = "" [@@bs.send.pipe: t]
+external add : string -> 'a -> unit = "Add" [@@bs.send.pipe: t]
+
+(** https://msdn.microsoft.com/en-us/library/a14xez73(v=vs.84).aspx *)
+module CompareMode : sig
+  val binary : int
+  val text : int
+  val database : int
+
+  external get : t -> int = "CompareMode" [@@bs.get]
+  external set : t -> int -> unit = "CompareMode" [@@bs.set]
+end
 
 (** https://msdn.microsoft.com/en-us/library/5t9h9579(v=vs.84).aspx *)
 external count : t -> int = "Count" [@@bs.get]
@@ -14,6 +24,9 @@ external exists : string -> bool = "Exists" [@@bs.send.pipe: t]
 
 (** https://msdn.microsoft.com/en-us/library/84k9x471(v=vs.84).aspx *)
 external item : string -> 'a = "Item" [@@bs.send.pipe: t]
+
+(** https://msdn.microsoft.com/en-us/library/45731e2w(v=vs.84).aspx *)
+external removeAll : t -> unit = "RemoveAll" [@@bs.send]
 
 (** https://msdn.microsoft.com/en-us/library/ywyayk03(v=vs.84).aspx *)
 external remove_exn : string -> unit = "Remove" [@@bs.send.pipe: t]
